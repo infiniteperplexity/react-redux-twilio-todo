95737fa101dd6dc0dd9113e2f0f4a682be1c37b0
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-01-10 16:00:54 -0800

    added sqlite database and express web app

---
+++ webapp/db.js
@@ -0,0 +1,54 @@
+const sqlite3 = require('sqlite3').verbose();
+let resources = [
+	[':Task','rdfs:subClassOf','rdfs:Class'],
+	[':isDueOn','rdfs:domain',':Task'],
+	[':isDueOn','rdfs:range','xsd:date'],
+	[':created','rdfs:domain',':Task'],
+	[':created','rdfs:range','xsd:date'],
+	[':isComplete','rdfs:domain',':Task'],
+	[':isComplete','rdfs:range','xsd:boolean'],
+	[':SubTasks','rdfs:subClassOf','rdf:Seq'],
+	[':hasSubTasks','rdfs:domain',':Task'],
+	[':hasSubTasks','rdfs:range',':SubTasks'],
+	[':TaskTag','rdfs:subClassOf','rdfs:Class'],
+	[':taggedAs','rdfs:domain',':Task'],
+	[':taggedAs','rdfs:range',':TaskTag']
+];
+let db = new sqlite3.Database('./db/todo.db', (err) => {
+  	if (err) {
+    	return console.error(err.message);
+  	}
+  	db.serialize(() => {
+  		let inserts = [];
+  		for (let triplet of resources) {
+  			let [s, p, o] = triplet;
+  			inserts.push('("'+s+'"');
+  			inserts.push('"'+p+'"');
+  			inserts.push('"'+o+'"');
+  			inserts.push('"resources")');
+  		}
+  		let insert = inserts.join(',');
+  		console.log(insert);
+		db
+		 .run(`CREATE TABLE IF NOT EXISTS quads (
+		 	subject text NOT NULL,
+		 	predicate text NOT NULL,
+		 	object text NOT NULL,
+		 	graph text NOT NULL
+		 )`)
+		 .run(`INSERT INTO quads (
+		 	subject,
+		 	predicate,
+		 	object,
+		 	graph)
+		 	VALUES `
+		 	+ insert + ';')
+		 .close((e) => {
+			if (err) {
+			    return console.error(err.message);
+			}
+			console.log('Close the database connection.');
+		});
+  		console.log('Connected to the on-disk SQlite database.');
+	});
+});
--- webapp/web.js
+++ webapp/web.js
@@ -1,8 +1,20 @@
-const port = 8080;
+const port = process.env.PORT || 8080;
+const path = require("path");
 const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/*.js', function (req, res) {
+   res.sendFile(path.join(__dirname, req.url));
+});
+
+app.get('/*.jsx', function (req, res) {
+   res.sendFile(path.join(__dirname, req.url));
+});
+
 
 app.listen(port, () => console.log('Example app listening on port'+port+'!'))
