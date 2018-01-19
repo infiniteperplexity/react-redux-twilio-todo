7d43d614c0fa95f2c85e64f9d8cc33a745a0d032
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-01-19 10:48:24 -0800

    got the database connection up and running!

--- todo/scripts/todo.jsx
+++ todo/scripts/todo.jsx
@@ -122,3 +122,54 @@ ReactDOM.render(
 	</ReactRedux.Provider>,
 	destination
 );
+
+
+
+let tasks;
+fetch('db.glenn',{}).then((res)=>{
+	if (res.status!==200) {
+        console.log('Looks like there was a problem. Status Code: '+res.status);
+        return;
+    }
+	res.json().then((data)=>{
+		tasks = data;	
+	});
+});
+
+
+let newtasks = [
+	[':Task','rdfs:subClassOf','rdfs:Class'],
+	[':isDueOn','rdfs:domain',':Task'],
+	[':isDueOn','rdfs:range','xsd:date'],
+	[':created','rdfs:domain',':Task'],
+	[':created','rdfs:range','xsd:date'],
+	[':isComplete','rdfs:domain',':Task'],
+	[':isComplete','rdfs:range','xsd:boolean'],
+	[':SubTasks','rdfs:subClassOf','rdf:Seq'],
+	[':hasSubTasks','rdfs:domain',':Task'],
+	[':hasSubTasks','rdfs:range',':SubTasks'],
+	[':TaskTag','rdfs:subClassOf','rdfs:Class'],
+	[':taggedAs','rdfs:domain',':Task'],
+	[':taggedAs','rdfs:range',':TaskTag']
+];
+
+function post() {
+	let pargs = {
+		method: 'POST',
+		headers: new Headers({'Content-Type': 'application/json;charset=UTF-8'}),
+		body: JSON.stringify(newtasks)
+	};
+	fetch('db.glenn',pargs).then((res)=>{
+		if (res.status!==200) {
+	        console.log('Looks like there was a problem. Status Code: '+res.status);
+	        return;
+	    }
+		res.json().then((data)=>{
+			tasks = data;
+			console.log(tasks);	
+		});
+	});
+}
+
+
+setTimeout(post, 2000);
--- todo/web.js
+++ todo/web.js
@@ -2,79 +2,71 @@ const port = process.env.PORT || 8080;
 const path = require("path");
 const express = require('express');
 const app = express();
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-
-const dbPromise = sqlite.open('./db/todo.db', {Promise});
+// assign and open database
+const db = new sqlite3.Database('./db/todo.db');
 
 app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/*.js', function(req, res) {
+app.get('/*.js*', function(req, res) {
    res.sendFile(path.join(__dirname, req.url));
 });
 
-app.get('/*.jsx', function(req, res) {
-
-
-
-res.sendFile(path.join(__dirname, req.url));
+app.post('/db.*', function(req, res) {
+	let user = req.url.split(".")[1];
+	let inserts = [];
+	for (let triplet of req.body) {
+  		let [s, p, o] = triplet;
+  		inserts.push('("'+s+'"');
+  		inserts.push('"'+p+'"');
+  		inserts.push('"'+o+'"');
+  		inserts.push('"'+user+'")');
+  	}
+  	let insert = inserts.join(',');
+	db.serialize(()=> {
+		db.run("DELETE FROM quads WHERE graph = ?",user,(err)=>{
+			if (err) {
+				console.log("had an error deleting rows.");
+				console.log(err);
+				res.status(404).send();
+			}
+		}).run('INSERT INTO quads (subject,predicate,object,graph) VALUES '+insert,(err)=>{
+			if (err) {
+				console.log("had an error inserting rows.");
+				console.log(err);
+				res.status(404).send();
+			}
+		}).all("SELECT * FROM quads WHERE graph = ?",user,(err, rows)=>{
+		// // .all('SELECT * FROM quads WHERE graph IN ("resources",?)',user,(err, rows)=>{
+			if (err) {
+				console.log("had an error retrieving updated rows.");
+				res.status(404).send();
+			}
+			console.log("sending updated rows...");
+			res.send(JSON.stringify(rows));
+		});
+	});
 });
 
-app.get('/sql.select.*', function(req, res) {
-	let db = await dbPromise;
-	
-	// pull the entire database at this point
-});
-
-
-
-
-// app.get('/post/:id', async (req, res, next) => {
-//   try {
-//     const db = await dbPromise;
-//     const [post, categories] = await Promise.all([
-//       db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
-//       db.all('SELECT * FROM Category')
-//     ]);
-//     res.render('post', { post, categories });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-app.post('/sql.insert.*', function(req, res) {
-	// insert into table
-});
-
-app.post('/sql.delete.*', function(req, res) {
-
+app.get('/db.*', function(req, res) {
+	let user = req.url.split(".")[1];
+	// ignore resources for now
+	db.all("SELECT * FROM quads WHERE graph = ?",user,(err, rows)=>{
+	// return all rows that are either for this user, or are for all users
+	//db.all("SELECT * FROM quads WHERE graph IN (?, 'resources')",user,(err, rows)=>{
+		if (err) {
+			console.log("had an error retrieving rows.");
+			res.status(404).send();
+		}
+		console.log("sending rows...");;
+		res.send(JSON.stringify(rows));
+	});
 });
 
 
 app.listen(port, () => console.log('Example app listening on port'+port+'!'))
-
-
-import express from 'express';
-import Promise from 'bluebird';
-import sqlite from 'sqlite';
- 
-// const app = express();
-// const port = process.env.PORT || 3000;
-// const dbPromise = sqlite.open('./database.sqlite', { Promise });
- 
-// app.get('/post/:id', async (req, res, next) => {
-//   try {
-//     const db = await dbPromise;
-//     const [post, categories] = await Promise.all([
-//       db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
-//       db.all('SELECT * FROM Category')
-//     ]);
-//     res.render('post', { post, categories });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
- 
-// app.listen(port);
