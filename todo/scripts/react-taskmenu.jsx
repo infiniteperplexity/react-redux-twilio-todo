ab0db00437d9984b6cfedd5aa04ab13dc60fad9a
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-01-30 12:16:04 -0800

    added even more drag and drop non-functional stuff

--- todo/scripts/react-taskmenu.jsx
+++ todo/scripts/react-taskmenu.jsx
@@ -9,29 +9,60 @@ function ListRadio({filter, id, setControl}) {
 }
 
 
-function TaskMenu({tasks, app, setControl}) {
-	if (!tasks) {
-		return null;
+class TaskMenu extends React.Component {
+	handleDrag = (event) => {
+		event.dataTransfer.setData("text", event.target.getAttribute("taskid"));
 	}
-	let statics = tasks.$Static.subtasks.map((list,i)=>(
-		<div key={i}>
-			<ListRadio id={list.id} filter={app.filter} setControl={setControl} />
-			{list.label}
-		</div>
-	));
-	let lists = tasks.$Lists.subtasks.map((list,i)=>(
-		<div key={i}>
-			<ListRadio id={list.id} filter={app.filter} setControl={setControl} />
-			{list.label}
-		</div>
-	));
-	return (
-		<div className="taskmenu appframe">
-			<form>
-				{statics}
-				<hr />
-				{lists}
-			</form>
-		</div>
-	);
+	allowDrop = (event) => {
+		event.preventDefault();
+	}
+	handleDrop = (event) => {
+		event.preventDefault();
+		let id = event.dataTransfer.getData("text");
+		let tasks = this.props.tasks;
+		let task1 = tasks[id];
+		let task2 = tasks[event.target.getAttribute("taskid")];
+		console.log("Dragged "+task1.label+" onto "+task2.label+".");
+	}
+	render() {
+		let tasks = this.props.tasks;
+		let app = this.props.app;
+		let setControl = this.props.setControl;
+		if (!tasks) {
+			return null;
+		}
+		let statics = tasks.$Static.subtasks.map((list,i)=>(
+			<div	key={i}
+					taskid={list.id}
+					draggable="true"
+					onDragStart={this.handleDrag}
+					onDragOver={this.allowDrop}
+					onDrop={this.handleDrop}
+			>
+				<ListRadio id={list.id} filter={app.filter} setControl={setControl} />
+				{list.label}
+			</div>
+		));
+		let lists = tasks.$Lists.subtasks.map((list,i)=>(
+			<div	key={i}
+					taskid={list.id}
+					draggable="true"
+					onDragStart={this.handleDrag}
+					onDragOver={this.allowDrop}
+					onDrop={this.handleDrop}
+			>
+				<ListRadio id={list.id} filter={app.filter} setControl={setControl} />
+				{list.label}
+			</div>
+		));
+		return (
+			<div className="taskmenu appframe">
+				<form>
+					{statics}
+					<hr />
+					{lists}
+				</form>
+			</div>
+		);
+	}
 }
