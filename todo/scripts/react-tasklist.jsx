27f534ebe085dbd85a0161cfa4448d5818dbf1a8
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-01-30 12:09:41 -0800

    drag and drop now works

--- todo/scripts/react-tasklist.jsx
+++ todo/scripts/react-tasklist.jsx
@@ -13,20 +13,45 @@ function TaskList(props) {
 		</div>
 	);
 }
-function TaskCard({app, label, id, showDetails, setControl, completeTask, sortTask, deleteTask}) {
-	return (
-		<div style={{overflow: "hidden"}}>
-			{label}
-			<span style={{float: "right"}}>
-				<TaskButton id={id} onClick={completeTask} tooltip="complete task">{"\u2713"}</TaskButton>
-				<TaskButton id={id} onClick={showDetails} tooltip="inspect/modify">{"?"}</TaskButton>
-				<TaskButton id={id} onClick={x=>setControl("filter",x)} tooltip="show subtasks">{"\u2261"}</TaskButton>			
-				<TaskButton id={id} onClick={x=>sortTask(x,app.filter,-1)} tooltip="sort task up">{"\u2191"}</TaskButton>
-				<TaskButton id={id} onClick={x=>sortTask(x,app.filter,+1)} tooltip="sort task down">{"\u2193"}</TaskButton>
-				<TaskButton id={id} onClick={deleteTask} tooltip="delete task">{"\u2717"}</TaskButton>
-			</span>
-		</div>
-	);
+
+class TaskCard extends React.Component {
+	handleDrag = (event) => {
+		event.dataTransfer.setData("text", event.target.getAttribute("taskid"));
+	}
+	allowDrop = (event) => {
+		event.preventDefault();
+	}
+	handleDrop = (event) => {
+		event.preventDefault();
+		let id = event.dataTransfer.getData("text");
+		let tasks = this.props.tasks;
+		let task1 = tasks[id];
+		let task2 = tasks[event.target.getAttribute("taskid")];
+		console.log("Dragged "+task1.label+" onto "+task2.label+".");
+	}
+	render() {
+		let id = this.props.id;
+		let app = this.props.app;
+		return (
+			<div	taskid={id}
+					draggable="true"
+					onDragStart={this.handleDrag}
+					onDragOver={this.allowDrop}
+					onDrop={this.handleDrop}
+					style={{overflow: "hidden"}}
+			>
+				{this.props.label}
+				<span style={{float: "right"}}>
+					<TaskButton id={id} onClick={this.props.completeTask} tooltip="complete task">{"\u2713"}</TaskButton>
+					<TaskButton id={id} onClick={this.props.showDetails} tooltip="inspect/modify">{"?"}</TaskButton>
+					<TaskButton id={id} onClick={x=>setControl("filter",x)} tooltip="show subtasks">{"\u2261"}</TaskButton>			
+					<TaskButton id={id} onClick={x=>sortTask(x,app.filter,-1)} tooltip="sort task up">{"\u2191"}</TaskButton>
+					<TaskButton id={id} onClick={x=>sortTask(x,app.filter,+1)} tooltip="sort task down">{"\u2193"}</TaskButton>
+					<TaskButton id={id} onClick={this.props.deleteTask} tooltip="delete task">{"\u2717"}</TaskButton>
+				</span>
+			</div>
+		);
+	}
 }
 
 function TaskButton({id, onClick, tooltip, children}) {
