10344167bd15ff76d1ea8a114e883a377f85d315
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-02-02 16:17:55 -0800

    almost complete

--- scripts/mobile-react.jsx
+++ scripts/mobile-react.jsx
@@ -2,10 +2,111 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-function App(props) {
-	let words = "Goodbye world";
-	return <p>{words}</p>;
+
+class Container extends React.Component {
+	render() {
+		return (
+			<div className="taskapp">
+				<p> Hola, world! </p>
+			</div>
+		);
+	}
 }
+/*
+					<TaskMenu {...this.props} />
+					<TaskDisplay {...this.props} />
+					<TaskModal {...this.props} />
+*/
+let App = ReactRedux.connect(
+	(state) => ({app: state.app, tasks: state.tasks}),
+	(dispatch) => ({
+		setControl: (control, value) => {
+			if (control==="filter" && value!==store.getState().app.filter) {
+				window.history.pushState({filter: value},"");
+			}
+			dispatch({type: "SET_CONTROL", control: control, value: value});
+		},
+		showDetails: (id) => dispatch({type: "SHOW_DETAILS", id: id}),
+		modifyTask: (task) => dispatch({type: "MODIFY_DATA", modify: [task]}),
+		deleteTask: (id) => dispatch({type: "MODIFY_DATA", delete: [id]}),
+		completeTask: (id) => {
+			let tasks = {...store.getState().tasks};
+			let task = {...tasks[id]};
+			if (task.repeats==="instantly") {
+				task.clicked = moment().unix();
+			} else {
+				task.completed = {
+					id: uuid.v4(),
+					moment: moment().unix(),
+					value: true
+				}
+			}
+			return dispatch({type: "MODIFY_DATA", modify: [task]});
+		},
+		changeList: (task, oldlist, newlist) => {
+			let noMove = ["$Everything","$Repeating","$Clickers","$Completed","$Static","$Inbox","$Lists"];
+			let noAdd = ["$Everything","$Repeating","$Clickers","$Completed","$Static"];
+			let noRemove = ["$Everything","$Repeating","$Clickers","$Completed","$Static"];
+			if (noMove.indexOf(task.id)!==-1 || noRemove.indexOf(oldlist.id)!==-1 || noAdd.indexOf(newlist.id)!==-1) {
+				console.log("don't be tryin' nothing crazy for now...");
+				return;
+			}
+			oldlist = {...oldlist};
+			newlist = {...newlist};
+			newlist.subtasks = newlist.subtasks || [];
+			let oldsub = [...oldlist.subtasks];
+			let newsub = [...newlist.subtasks];
+			oldsub.splice(oldsub.indexOf(task),1);
+			newsub.push(task);
+			oldlist.subtasks = oldsub;
+			newlist.subtasks = newsub;
+			return dispatch({type: "MODIFY_DATA", modify: [oldlist, newlist]});
+		},
+		addTask: (args) => {
+			args = args || {};
+			let tasks = store.getState().tasks;
+			let task = {
+				id: args.id || uuid.v4(),
+				created: args.moment || moment().unix(),
+				inputs: args.inputs || "check"
+			};
+			task = {...task, ...args};
+			let inbox = {...tasks.$Inbox, subtasks: tasks.$Inbox.subtasks.concat(task)};
+			let memberships = [];
+			if (args.memberof) {
+				let tasks = store.getState().tasks;
+				for (let listid of args.memberof) {
+					tasks[listid].subtasks = tasks[listid].subtasks || [];
+					let list = {...tasks[listid], subtasks: tasks[listid].subtasks.concat(task)};
+					memberships.push(list);
+				}
+				delete task.memberof;
+			}
+			return dispatch({type: "MODIFY_DATA", add: [task], modify: memberships});
+		},
+		sortTask: (taskid, listid, n) => {
+			// immediately sort all completed tasks to the bottom?
+			let task = store.getState().tasks[taskid];
+			let list = store.getState().tasks[listid];
+			let index = list.subtasks.indexOf(task);
+			if (index+n < 0 || index+n >= list.subtasks.length) {
+				return;
+			} else {
+				let newlist = [];
+				for (let item of list.subtasks) {
+					newlist.push(item);
+				}
+				newlist[index] = newlist[index+n];
+				newlist[index+n] = task;
+				let incomplete = newlist.filter(t=>!t.completed);
+				let completed = newlist.filter(t=>t.completed);
+				newlist = incomplete.concat(completed);
+				return dispatch({type: "MODIFY_DATA", modify: [{...list, subtasks: newlist}], });
+			}
+		}
+	})
+)(Container);
+
 window.onpopstate = function(event) {
    	if (event.state) {
   		store.dispatch({type: "SET_CONTROL", control: "filter", value: event.state.filter});
@@ -13,11 +114,6 @@ window.onpopstate = function(event) {
 };
 window.history.replaceState({filter: "$Inbox"},"title", window.location);
 let destination = document.querySelector("#container");
-// ReactDOM.render(
-// 	<App />,
-// 	destination
-// );
-
 ReactDOM.render(
 	<ReactRedux.Provider store={store}>
 		<App />
