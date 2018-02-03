6a1ce59d1de844b8730c5cf04021a4dc886590d0
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-02-02 16:07:03 -0800

    trying to break the mobile react page

--- scripts/mobile-react.jsx
+++ scripts/mobile-react.jsx
@@ -1,6 +1,16 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
 function App(props) {
 	return <p>Hello world</p>;
 }
+window.onpopstate = function(event) {
+   	if (event.state) {
+  		store.dispatch({type: "SET_CONTROL", control: "filter", value: event.state.filter});
+  	}
+};
+window.history.replaceState({filter: "$Inbox"},"title", window.location);
 let destination = document.querySelector("#container");
 ReactDOM.render(
 	<App />,
