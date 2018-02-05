4b5f5e725da3c29296cb4a3b5a59d9fcf1215696
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-02-05 14:09:23 -0800

    contract the testing

--- scripts/react-mobile.jsx
+++ scripts/react-mobile.jsx
@@ -1,48 +1,48 @@
-class MobileMenu extends React.Component {
-	handleChange = (event)=> {
-		this.props.setControl("filter",event.target.value);
-	}
-	render() {
-		let tasks = this.props.tasks;
-		let app = this.props.app;
-		let setControl = this.props.setControl;
-		if (!tasks) {
-			return null;
-		}
-		let statics = tasks.$Static.subtasks.map((list,i)=>
-			<option key={i} value={list.id}>{list.label}</option>
-		);
-		let lists = tasks.$Lists.subtasks.map((list,i)=>
-			<option key={i} value={list.id}>{list.label}</option>
-		);
-		return (
-			<div>
-				<select value={this.props.app.filter}
-              			onChange={this.handleChange}
-      			>
-	      			{statics}
-	      			<option disabled="true">---------------------</option>
-	      			{lists}
-      			</select>
-			</div>
-		);
-	}
-}
-
 // class MobileMenu extends React.Component {
+// 	handleChange = (event)=> {
+// 		this.props.setControl("filter",event.target.value);
+// 	}
 // 	render() {
 // 		let tasks = this.props.tasks;
-//  		if (!tasks) {
-//  			return null;
-//  		}
+// 		let app = this.props.app;
+// 		let setControl = this.props.setControl;
+// 		if (!tasks) {
+// 			return null;
+// 		}
+// 		let statics = tasks.$Static.subtasks.map((list,i)=>
+// 			<option key={i} value={list.id}>{list.label}</option>
+// 		);
+// 		let lists = tasks.$Lists.subtasks.map((list,i)=>
+// 			<option key={i} value={list.id}>{list.label}</option>
+// 		);
 // 		return (
 // 			<div>
-// 				<select>
-// 					<option value="testing">Test static</option>
+// 				<select value={this.props.app.filter}
+//               			onChange={this.handleChange}
+//       			>
+// 	      			{statics}
 // 	      			<option disabled="true">---------------------</option>
-// 	      			<option value="foo">foo</option>
+// 	      			{lists}
 //       			</select>
 // 			</div>
 // 		);
 // 	}
 // }
+
+class MobileMenu extends React.Component {
+	render() {
+		let tasks = this.props.tasks;
+ 		if (!tasks) {
+ 			return null;
+ 		}
+		return (
+			<div>
+				<select>
+					<option value="testing">Test static</option>
+	      			<option disabled="true">---------------------</option>
+	      			<option value="foo">foo</option>
+      			</select>
+			</div>
+		);
+	}
+}
