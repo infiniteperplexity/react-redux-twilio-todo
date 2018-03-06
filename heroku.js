f2bdd200234aaa125311795e14cca4b717448711
infiniteperplexity <infinite.perplexity@gmail.com>
2018-03-05 21:07:41 -0800

    fiddled with comments a bit

--- heroku.js
+++ heroku.js
@@ -22,26 +22,6 @@ const bodyParser = require('body-parser');
 // }).then(data=>console.log("message sent."))
 //   .catch(err=>console.log(err));
 
-
-// var config = require('./config');
-
-// module.exports.sendSms = function(to, message) {
-//   var client = require('twilio')(config.accountSid, config.authToken);
-//   // console.log(client.api.messages.create())
-//   return client.api.messages
-//     .create({
-//       body: message,
-//       to: to,
-//       from: config.sendingNumber,
-//     }).then(function(data) {
-//       console.log('Administrator notified');
-//     }).catch(function(err) {
-//       console.error('Could not notify administrator');
-//       console.error(err);
-//     });
-// };
-
-
 const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 
