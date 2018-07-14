1bb43b28937ecd450ec5340f33cddd60e03894b2
infiniteperplexity <infinite.perplexity@gmail.com>
2018-07-13 22:02:30 -0700

    fixed zero weekly entries thing

--- heroku.js
+++ heroku.js
@@ -236,6 +236,9 @@ function generateReport(tasks) {
       let repeat = "Summary for " + task.label + ": "
       + "\n  Weekly total: " + task.summaries.weeklyTotal
       + "\n  Weekly average: " + (task.summaries.weeklyTotal/task.summaries.weeklyDays).toFixed(2);
+      if (task.summaries.weeklyDays===0) {
+        repeats = "No weekly entries for " + task.label + ".";
+      }
       repeats.push(repeat);
     }
   }
